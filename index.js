0ebd4a0786ed6dac117b0702c74635bb33e94a85
priyad16 <priyadarshinikanchipuram@gmail.com>
2025-11-23 00:12:39 +0530

    add route for connections

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const connectToDB=require('./DBConnection/ConnectToDB');
 const express=require('express');
+const cookieParser=require('cookie-parser')
+
 const app=express();
+
 app.use(express.json());
+app.use(cookieParser())
+
 const authrouter=require('./routes/auth.router')
+const connectionrouter=require('./routes/connection.router')
+
 app.use("/auth",authrouter);
+app.use("/connection",connectionrouter);
+
 connectToDB()
 .then(()=>{
     console.log("connection established sucesssfully");
